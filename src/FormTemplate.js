343659932ca33558ff750aaff97584132c924aa4
ixyhoo <ixyhoo@outlook.fr>
2023-09-07 10:04:29 +0200

    try again FormTemplate

--- src/FormTemplate.js
+++ src/FormTemplate.js
@@ -1,31 +1,10 @@
-import React,{ useRef }from 'react';
-import emailjs from '@emailjs/browser';
+import React from 'react';
 
- const FormTemplate = () => {
-  const form = useRef;
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      
-      });
-  };
-
+const FormTemplate = () => {
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="name" />
-      <label>Email</label>
-      <input type="email" name="email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Envoyer" />
-    </form>
+    <div>
+      
+    </div>
   );
 };
 
