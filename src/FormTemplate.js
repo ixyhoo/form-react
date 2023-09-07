e20246950c0ff40b93c10978fda6566294118885
ixyhoo <ixyhoo@outlook.fr>
2023-09-07 09:57:55 +0200

    error export default

--- src/FormTemplate.js
+++ src/FormTemplate.js
@@ -1,8 +1,8 @@
-import React, { useRef } from 'react';
+import React,{ useRef }from 'react';
 import emailjs from '@emailjs/browser';
 
-export const ContactUs = () => {
-  const form = useRef();
+ const FormTemplate = () => {
+  const form = useRef;
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -12,18 +12,21 @@ export const ContactUs = () => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
+      
       });
   };
 
   return (
     <form ref={form} onSubmit={sendEmail}>
       <label>Name</label>
-      <input type="text" name="user_name" />
+      <input type="text" name="name" />
       <label>Email</label>
-      <input type="email" name="user_email" />
+      <input type="email" name="email" />
       <label>Message</label>
       <textarea name="message" />
-      <input type="submit" value="Send" />
+      <input type="submit" value="Envoyer" />
     </form>
   );
 };
+
+export default FormTemplate;
